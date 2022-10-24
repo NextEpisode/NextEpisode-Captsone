22a8d53d91f0f0541671ca6e9d6f9224b2d0e31e
Christian Acevedo <christian.acevedo14@upr.edu>
2022-10-23 22:32:02 -0400

    Added tables to display the users movie and series katalogues.

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -9,6 +9,7 @@ import ForumItem from './ForumItem';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
+import BasicTable from './Table';
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -45,27 +46,55 @@ function a11yProps(index: number) {
 
 function BasicTabs() {
   const [value, setValue] = React.useState(0);
+  let isMovie = true;
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
   return (
-    <Box sx={{ width: '100%', mt:10}}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider'}}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" sx={{ ml:100}}>
+    <Box sx={{ width: '100%', mt: 10 }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" sx={{ ml: 100 }}>
           <Tab label="Movies" {...a11yProps(0)} />
           <Tab label="Series" {...a11yProps(1)} />
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
-        <MovieKatalogue />
+        <Katalogue isMovie={true} medias={[{
+          title: 'Shrek',
+          poster_path: 'https://www.themoviedb.org/t/p/original/iB64vpL3dIObOtMZgX3RqdVdQDc.jpg',
+          name: 'Shrek name',
+          release_date: '2001'
+        },{
+          title: 'Dune',
+          poster_path: 'https://imageio.forbes.com/specials-images/imageserve/61116cea2313e8bae55a536a/-Dune-/0x0.jpg?format=jpg&width=960',
+          name: 'Dune name',
+          release_date: '2022',
+        },{
+          title: 'Robinhood',
+          poster_path: 'https://movieposters2.com/images/1595344-b.jpg',
+          name: 'Robinhood name',
+          release_date: '2099'
+        }]}/>
       </TabPanel>
       <TabPanel value={value} index={1}>
-        <SeriesKatalogue />
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
+        <Katalogue isMovie={false} medias={[{
+          title: 'House of Dragons',
+          poster_path: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/z2yahl2uefxDCl0nogcRBstwruJ.jpg',
+          name: 'House of dragons name',
+          release_date: '2022'
+        },{
+          title: 'Lord of the Rings - Rings of Power',
+          poster_path: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/mYLOqiStMxDK3fYZFirgrMt8z5d.jpg',
+          name: 'Rings of Power',
+          release_date: '2099'
+        },{
+          title: 'Chainsaw Man',
+          poster_path: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/npdB6eFzizki0WaZ1OvKcJrWe97.jpg',
+          name: 'Chainsaw name',
+          release_date: '2022',
+        }]}/>
       </TabPanel>
     </Box>
   );
@@ -73,60 +102,22 @@ function BasicTabs() {
 
 
 
-function MovieKatalogue(){
-
-    return(
-      <div>
-    <Carousel >
-                                    <CarouselItem media={{
-        title : 'Shrek',
-        poster_path : 'https://www.themoviedb.org/t/p/original/iB64vpL3dIObOtMZgX3RqdVdQDc.jpg',
-        name : 'Shrek name',
-        release_date : '2001'
-      }} />
-                                    <CarouselItem media={{
-            title : 'Robinhood',
-            poster_path : 'https://movieposters2.com/images/1595344-b.jpg',
-            name : 'Robinhood name',
-            release_date : '2099'
-        }}/>
-                                    <CarouselItem media={{
-                title : 'Dune',
-                poster_path : 'https://imageio.forbes.com/specials-images/imageserve/61116cea2313e8bae55a536a/-Dune-/0x0.jpg?format=jpg&width=960',
-                name : 'Dune name',
-                release_date : '2022',
-            }}/>
-      </Carousel>
-      </div>
-    
-    )
-}
-
-function SeriesKatalogue(){
-  return(
+function Katalogue({medias,isMovie}) {
+  return (
     <div>
-  <Carousel >
-                                  <CarouselItem media={{
-      title : 'House of Dragons',
-      poster_path : 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/z2yahl2uefxDCl0nogcRBstwruJ.jpg',
-      name : 'House of dragons name',
-      release_date : '2022'
-    }} />
-                                  <CarouselItem media={{
-          title : 'Lord of the Rings - Rings of Power',
-          poster_path : 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/mYLOqiStMxDK3fYZFirgrMt8z5d.jpg',
-          name : 'Rings of Power',
-          release_date : '2099'
-      }}/>
-                                  <CarouselItem media={{
-              title : 'Chainsaw Man',
-              poster_path : 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/npdB6eFzizki0WaZ1OvKcJrWe97.jpg',
-              name : 'Chainsaw name',
-              release_date : '2022',
-          }}/>
-    </Carousel>
+      <Carousel>
+        {medias.map((media) => (
+          <CarouselItem media={media} />
+        ))}
+      </Carousel>
+      <Container>
+        <Typography variant="h4" >
+          Katalogue
+        </Typography>
+        <BasicTable medias={medias} isMovie={isMovie} />
+      </Container>
     </div>
-  
+
   )
 }
 
---
+++ frontend/src/components/Table.js
@@ -0,0 +1,105 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { CardMedia } from '@mui/material';
+
+
+
+
+export default function BasicTable({medias,isMovie}) {
+    if(isMovie){
+        return MovieTable({medias});
+    }
+    else{
+        return SeriesTable({medias});
+    }
+}
+
+function MovieTable({medias}){
+    return(
+<TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Poster </TableCell>
+            <TableCell align="left">Name</TableCell>
+            <TableCell align="right">Title</TableCell>
+            <TableCell align="right">Release Date</TableCell>
+            <TableCell align="right">Category</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {medias.map((media) => (
+            <TableRow
+              key={media.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="right">
+                <CardMedia
+                    component="img"
+                    height="100"
+                    width="100"
+                    image={media.poster_path}
+                />
+              </TableCell>
+              <TableCell component="th" scope="row">
+                {media.name}
+              </TableCell>
+              <TableCell align="right">{media.title}</TableCell>
+              <TableCell align="right">{media.release_date}</TableCell>
+
+              <TableCell align="right">{media.name}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    );
+}
+
+function SeriesTable({medias}){
+    return(
+        <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Poster </TableCell>
+            <TableCell align="left">Name</TableCell>
+            <TableCell align="right">Title</TableCell>
+            <TableCell align="right">Release Date</TableCell>
+            <TableCell align="right">Episode</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {medias.map((media) => (
+            <TableRow
+              key={media.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="right">
+                <CardMedia
+                    component="img"
+                    height="100"
+                    width="100"
+                    image={media.poster_path}
+                />
+              </TableCell>
+              <TableCell component="th" scope="row">
+                {media.name}
+              </TableCell>
+              <TableCell align="right">{media.title}</TableCell>
+              <TableCell align="right">{media.release_date}</TableCell>
+
+              <TableCell align="right">{media.name}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    );
+}
