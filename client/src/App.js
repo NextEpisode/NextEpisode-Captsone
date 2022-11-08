85dbef12fa6de05f4e447ad6b00ad77bb0b4b241
Christian Acevedo <christian.acevedo14@upr.edu>
2022-11-07 20:11:40 -0400

    removing unused imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import SignIn from './components/SignIn';
-import NavBar from './components/NavBar';
 import Profile from './components/Profile';
 import {Search} from "./components/Search";
 import {MediaPage} from "./components/MediaPage";
